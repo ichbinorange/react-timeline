099062553535e0488a6b62c91837191354fd3ced
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-22 20:08:31 -0800

    added method to Timeline.js, revised App.js and passed the tests for wave2

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
   
-  return;
+  return (
+    props.events.map((evt) => {
+      return (
+        <div className="timeline">
+          <TimelineEvent 
+            person={evt.person}
+            status={evt.status}
+            timeStamp={evt.timeStamp} />
+        </div>
+      )
+    })
+  );
 }
 
 export default Timeline;
