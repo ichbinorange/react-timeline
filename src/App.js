8aa325e79850d2acdc8fe556f3d76a403e4824b5
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-22 19:56:14 -0800

    hardcoded data in App.js, added method in TimelineEvent.js and passed the tests for wave1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 function App() {
   console.log(timelineData);
@@ -11,9 +12,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="App-title">Application title</h1>
+        <h1 className="App-title">{timelineData.person}'s social media feed</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent
+          person="Ada Lovelace"
+          status="The Original programmer... old-school style, on PAPER."
+          timeStamp="2018-05-18T22:12:03Z" />
       </main>
     </div>
   );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  
-  return;
+
+const TimelineEvent = (props) => {
+  return (
+    <div className="timeline-event timeline-event:hover">
+      <p className="event-person">{props.person}</p>
+      <p className="event-status">{props.status}</p>
+      <p className="event-time"><Timestamp time={props.timeStamp} /></p>
+    </div>
+  );
 }
 
 export default TimelineEvent;
